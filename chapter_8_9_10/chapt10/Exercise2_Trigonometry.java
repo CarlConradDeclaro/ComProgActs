6132d70b170d9a4a9ef49f61c790f3bb02892b0d
CarlConradDeclaro <110441309+CarlConradDeclaro@users.noreply.github.com>
2023-02-13 17:53:53 -0800

    Add files via upload

---
+++ chapter_8_9_10/chapt10/Exercise2_Trigonometry.java
@@ -0,0 +1,21 @@
+class Exercise2_Trigonometry {
+
+    public static void main(String[] args) {
+    
+    
+       
+    double value = 0.5236;
+    
+    double sinx = Math.sin(value);
+    double cosx = Math.cos(value);
+    double sum = sinx*sinx + cosx*cosx;
+
+    
+    System.out.println("sine: " + sinx + "\ncosine: " + cosx + "\nsum of squares: " + sum );
+    
+    
+    
+    
+     
+     }
+}
