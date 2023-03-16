194a631d2a4bec83832abb3f781d26344ce6c767
CarlConradDeclaro <110441309+CarlConradDeclaro@users.noreply.github.com>
2023-03-15 17:25:29 -0700

    Update LastChanceGas.java

--- chapter_8_9_10/chapt16/LastChanceGas.java
+++ chapter_8_9_10/chapt16/LastChanceGas.java
@@ -1,6 +1,6 @@
- import java.util.Scanner;  
+import java.util.Scanner;  
 
-  class Main {
+  class LastChanceGas {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
    
@@ -18,11 +18,41 @@
          MilesPerGallon = sc.nextInt();
     
          int remainingMiles = (Tankcapacity*Gage /100)* MilesPerGallon;
-         if(remainingMiles<200){
+         if(remainingMiles<=200){
               System.out.println("\nGet Gas! ");
-         }
-         if(remainingMiles>=200){
+         }else 
              System.out.println("\nSafe to Proceed ");
-         }
+ 
   }
 }
+
+
+
+//  import java.util.Scanner;  
+
+//   class Main {
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+   
+//          int Tankcapacity;
+//          int Gage;
+//          int MilesPerGallon;
+         
+//          System.out.println("Tank capacity: ");
+//          Tankcapacity = sc.nextInt();
+         
+//          System.out.println("Gage reading: ");
+//          Gage = sc.nextInt();
+         
+//          System.out.println("Miles per gallon: ");
+//          MilesPerGallon = sc.nextInt();
+    
+//          int remainingMiles = (Tankcapacity*Gage /100)* MilesPerGallon;
+//          if(remainingMiles<200){
+//               System.out.println("\nGet Gas! ");
+//          }
+//          if(remainingMiles>=200){
+//              System.out.println("\nSafe to Proceed ");
+//          }
+//   }
+// }
