85741eefbd216a481617c78e6fdf8260aba6aecf
CarlConradDeclaro <110441309+CarlConradDeclaro@users.noreply.github.com>
2023-03-21 19:43:30 +0800

    Delete chapter_8_9_10 directory

--- chapter_8_9_10/chapt8/Shortfall.java
+++
@@ -1,8 +0,0 @@
-public class Shortfall {
-    public static void main(String[] args) {
-
-        // short value = 35000;// incompatible types: possible lossy conversion from int to short
-        int value = 35000;
-        System.out.println("A short: " + value);
-    }
-}
