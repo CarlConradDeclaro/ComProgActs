98ac7132bd0f4f065ca32d73a0a2325cf686ba3c
Carl Conrad C. Declaro <110441309+CarlConradDeclaro@users.noreply.github.com>
2023-09-27 22:18:43 +0800

    Delete chapt8 directory

--- chapt8/DoubleJeopardy.java
+++
@@ -1,9 +0,0 @@
-public class DoubleJeopardy {
-    public static void main(String[] args){
-    
-        double value = 32;// the value is 32.0
-       // double value = 2.7E308; // error: integer number too large
-        System.out.println("A double: " + value);
-        
-    }
-}
--- chapt8/ExponentialExplosion.java
+++
@@ -1,9 +0,0 @@
-public class ExponentialExplosion {
-    public static void main(String[] args){
-    
-        // double value = 32;
-        double value = 710; // infinity
-        System.out.println("e to the power value: " + Math.exp( value ) );
-   
-    }
-}
