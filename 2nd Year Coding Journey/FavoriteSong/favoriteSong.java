0d110f46eb91cce2106b7817f65c50255ae5a12e
carl conrad <carlconrad98071234@gmail.com>
2023-09-28 00:08:45 +0800

    song lyrics

---
+++ 2nd Year Coding Journey/FavoriteSong/favoriteSong.java
@@ -0,0 +1,16 @@
+class FavoriteLines{
+      String one = "\"I know you're sad and tired\"";
+      String two = "\"You've got nothing left to give \"";
+      String three = "\"You'll find another life to live\"";
+      String four = "\"I know that you'll get over it\"";
+      FavoriteLines(){
+        System.out.println(one+"\n"+ two +"\n"+ three +"\n"+ four );         
+      }
+}
+public class favoriteSong{
+        public static void main(String[] args) {
+           
+           FavoriteLines favLines =  new FavoriteLines();
+           
+        }
+}
