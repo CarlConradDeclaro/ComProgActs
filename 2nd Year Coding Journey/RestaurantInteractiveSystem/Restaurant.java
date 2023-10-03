1fb9c9cfa57edbd13d66780af2347a3958fac882
carl conrad <carlconrad98071234@gmail.com>
2023-10-03 14:14:43 +0800

    restaurant

--- 2nd Year Coding Journey/RestaurantInteractiveSystem/Restaurant.java
+++ 2nd Year Coding Journey/RestaurantInteractiveSystem/Restaurant.java
@@ -1,21 +1,41 @@
- import java.util.*;
+  import java.util.*;
 
 class Store{
-     
- 
-    void print(){
-        ArrayList<Product> prods = new ArrayList<>();
+      
+  ArrayList<Product> products  = new ArrayList<>();
+   
 
-        for(Product producds : prods){
-          System.out.println(producds.id +" "+ producds.productId +" "+ producds.prodPrice);
-          System.out.println("s");
+
+
+    
+   void defaultProd(){
+    Product prod1 = new Product(1,"C1 - PHP", 100.00);
+    Product prod2 = new Product(2,"C2 - PHP", 150.00);
+    Product prod3 = new Product(3,"C3 - PHP", 200.00);
+    
+    Product addOns1 = new Product(4,"R1 - PHP", 35.00);
+    Product addOns2 = new Product(5,"R2 - PHP", 50.00);
+    products.add(prod1);
+    products.add(prod2);
+    products.add(prod3);
+    products.add(addOns1);
+    products.add(addOns2);
+  
+}
+
+
+
+ void print(){
+    
+        
+        System.out.println("Menu");
+        for(Product prodt : products){
+          System.out.println(prodt.id +" "+ prodt.productId +" "+ prodt.prodPrice);         
         }
         
-    }
- 
+       }
 
- 
-  
+
 
 }
 
@@ -25,7 +45,7 @@ class Product extends Store{
    String productId;
    double prodPrice;
 
-   ArrayList<String> products  = new ArrayList<>();
+  
    Product(){}
    public Product(int id,String productId,double prodPrice ){
     this.id =id;
@@ -34,45 +54,41 @@ class Product extends Store{
    }
    
 
-   void defaultProd(){
-    Product prod1 = new Product(1,"C1 - PHP", 100.00);
-    Product prod2 = new Product(2,"C2 - PHP", 150.00);
-    Product prod3 = new Product(3,"C3 - PHP", 200.00);
-    
-    Product addOns1 = new Product(4,"R1 - PHP", 35.00);
-    Product addOns2 = new Product(5,"R2 - PHP", 50.00);
-    products.add(prod1.toString());
-    products.add(prod2.toString());
-    products.add(prod3.toString());
-    products.add(addOns1.toString());
-    products.add(addOns2.toString());
-  
-}
 }
 
 
-class Order extends Store{
+class Order {
 
 
    public void order(){
      
-
-
    }
 
 
 }
 
-
-
-
-
-class Main{
+class Restaurant{
     public static void main(String[] args) {
-       Store a =new Store();
-       a = new Product();
+        Scanner sc =  new Scanner(System.in);
+       Store a = new Product();
        a.defaultProd();
-       
        a.print();
+       
+       
+          
+          boolean run = true;
+        do{
+        
+            System.out.println("Enter product: ");
+            int choice = sc.nextInt();
+            
+            
+         
+       
+        }while(run);
+       
+       
+      
+
     }
 }
