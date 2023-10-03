6d68204b4f8616007741933b8f1fe6afa08b6ddc
Carl Conrad C. Declaro <110441309+CarlConradDeclaro@users.noreply.github.com>
2023-10-03 02:15:02 +0000

    restaurant

--- 2nd Year Coding Journey/RestaurantInteractiveSystem/Restaurant.java
+++ 2nd Year Coding Journey/RestaurantInteractiveSystem/Restaurant.java
@@ -5,15 +5,22 @@ class Store{
      
     ArrayList<Product> products  = new ArrayList<>();
     
-  Product prod1 = new Product(1,"C1 - PHP", 100.00);
-  Product prod2 = new Product(2,"C2 - PHP", 150.00);
-  Product prod3 = new Product(3,"C3 - PHP", 200.00);
-  
-  Product addOns1 = new Product(4,"R1 - PHP", 35.00);
-  Product addOns2 = new Product(5,"R2 - PHP", 50.00);
-
-
-   products.add(prod1);
+ 
+    void defaultProd(){
+        Product prod1 = new Product(1,"C1 - PHP", 100.00);
+        Product prod2 = new Product(2,"C2 - PHP", 150.00);
+        Product prod3 = new Product(3,"C3 - PHP", 200.00);
+        
+        Product addOns1 = new Product(4,"R1 - PHP", 35.00);
+        Product addOns2 = new Product(5,"R2 - PHP", 50.00);
+        products.add(prod1);
+        products.add(prod2);
+        products.add(prod3);
+        products.add(addOns1);
+        products.add(addOns2);
+      
+    }
+ 
   
 
 }
