56e683700842d9558ba5308f53dbe711b6abef40
Carl Conrad C. Declaro <110441309+CarlConradDeclaro@users.noreply.github.com>
2023-10-12 00:02:05 +0800

    Update RoutesToMoalboal.java

--- 2nd Year Coding Journey/FastestRoutesToMoaboal/RoutesToMoalboal.java
+++ 2nd Year Coding Journey/FastestRoutesToMoaboal/RoutesToMoalboal.java
@@ -6,6 +6,36 @@ class FindShorstestRoutes{
         List<String> obstructed = new ArrayList<>();
         private double speedy =0;
 
+   
+    void getObstrutedRoutes(){                 
+             System.out.println("Is Barili obstructed? (y/n) ");
+             String obs = sc.nextLine().toLowerCase();             
+                if(obs.equals("y")){
+                    obstructed.add("Barili");
+                    System.out.println("Is Dumanjug obstructed? (y/n) ");
+                    String ans = sc.nextLine().toLowerCase();
+                    if(ans.equals("y"))
+                        obstructed.add("Dumanjug");                 
+                } 
+     }
+
+     void askSpeed(){
+             System.out.println("Enter Speed ");
+             speedy = sc.nextDouble();
+     }
+ 
+    void displayRoutes(){
+        System.out.println("\t <<< Best Routes >>>\n");   
+        for(Routes r : routes){
+            if(!(r.startingPoint.contains("SouthBus")))
+                 System.out.printf("%-15s %-7s %-15s %.1f%n",r.startingPoint,"->",r.route,r.distance);
+            else  
+                System.out.printf("%-15s %-7s %-15s%n",r.startingPoint,"->",r.route);
+            if(r.distination.contains("Moalboal"))   
+               System.out.println(r.distination+"\t"+r.distance+"km");   
+        }       
+    }
+ 
      void findBestRoutes(){   
 
                getObstrutedRoutes();
@@ -69,33 +99,6 @@ class FindShorstestRoutes{
         return dis;
     }
 
-    void getObstrutedRoutes(){                 
-             System.out.println("Is Barili obstructed? (y/n) ");
-             String obs = sc.nextLine().toLowerCase();             
-                if(obs.equals("y")){
-                    obstructed.add("Barili");
-                    System.out.println("Is Dumanjug obstructed? (y/n) ");
-                    String ans = sc.nextLine().toLowerCase();
-                    if(ans.equals("y"))
-                        obstructed.add("Dumanjug");                 
-                } 
-     }
-
-     void askSpeed(){
-             System.out.println("Enter Speed ");
-             speedy = sc.nextDouble();
-     }
-    void displayRoutes(){
-        System.out.println("\t <<< Best Routes >>>\n");   
-        for(Routes r : routes){
-            if(!(r.startingPoint.contains("SouthBus")))
-                 System.out.printf("%-15s %-7s %-15s %.1f%n",r.startingPoint,"->",r.route,r.distance);
-            else  
-                System.out.printf("%-15s %-7s %-15s%n",r.startingPoint,"->",r.route);
-            if(r.distination.contains("Moalboal"))   
-               System.out.println(r.distination+"\t"+r.distance+"km");   
-        }       
-    }
 }
 
 class Routes{
