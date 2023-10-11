22247a49935e7b3b20e3c7a0d31df058685fe2a7
carl conrad <carlconrad98071234@gmail.com>
2023-10-11 18:07:28 +0800

    best routes

--- 2nd Year Coding Journey/FastestRoutesToMoaboal/RoutesToMoalboal.java
+++ 2nd Year Coding Journey/FastestRoutesToMoaboal/RoutesToMoalboal.java
@@ -5,10 +5,8 @@ class RoutesDistance{
         List<Routes> routes = new ArrayList<>();    
         List<String> obstructed = new ArrayList<>();
 
-    double computeTotalDistance(double speed, double dis){
-            double d = dis;
-              return (d/ speed  ) * 60;
-     }
+    double computeTotalDistance(double speed, double dis){return (dis/ speed  ) * 60;}
+
     double getDisTance(){
         double dis=0;
         for(Routes d : routes)
@@ -16,45 +14,55 @@ class RoutesDistance{
         return dis;
     }
     
-     void printRoutes(){
-           
-            
-             double TOA =0;
-             
+    void displayRoutes(){
+        System.out.println("\t <<< Best Routes >>>");
+        System.out.println("\nCebu city \t -> \tStart");
+        System.out.println("Minglanilia \t -> \tRoute 1");
+        System.out.println("San Fernando \t -> \tRoute 2");
+        for(Routes r : routes){
+            if(!(r.startingPoint.equals("SouthBus")))
+               System.out.println(r.startingPoint);   
+            if(r.distination.contains("Moalboal"))   
+               System.out.println(r.distination);   
+        }       
+    }
+     void bestRoutes(){    
+                               
              System.out.println("Is Barili obstructed? (y/n) ");
-             String obs = sc.nextLine().toLowerCase();
-             
+             String obs = sc.nextLine().toLowerCase();             
              if(obs.equals("y")){
                  obstructed.add("Barili");
-                  System.out.println("Is Dumanjug obstructed? (y/n) ");
-                  String ans = sc.nextLine().toLowerCase();
-                  if(ans.equals("y")){
-                       obstructed.add("Dumanjug");
-                  }
+                    System.out.println("Is Dumanjug obstructed? (y/n) ");
+                    String ans = sc.nextLine().toLowerCase();
+                        if(ans.equals("y")){
+                            obstructed.add("Dumanjug");
+                        }
              }
              
-             System.out.println("Enter Speed ");
-             double speedy = sc.nextDouble();
+               System.out.println("Enter Speed ");
+               double speedy = sc.nextDouble();
              
                Routes route1_2_3 = new Routes("SouthBus", "Carcar",38.3);
               
-               Routes route3_41 = new Routes("CarCar","barili",17.8);
-               Routes route41_411 = new Routes("barili","dumanjug",16.9);
-               Routes route411_412 = new Routes("Dumanjug","Moalboal",13.3);
+               Routes route3_41 = new Routes("CarCar\t\t -> \tRoute 3","barili",17.8);
+               Routes route41_411 = new Routes("barili\t\t -> \tRoute 4.1","dumanjug ",16.9);
+               Routes route411_412 = new Routes("Dumanjug\t -> \tRoute 4.1.1","Moalboal\t -> \tRoute 4.1.2\n",13.3);
               
-               Routes route42_421 = new Routes("sibonga","dumanjug",28.5);
-               Routes route421_422 = new Routes("dumanjug","Moalboal",18.9);
+               Routes route42_421 = new Routes("sibonga\t\t -> \tRoute 4.2","dumanjug",28.5);
+               Routes route421_422 = new Routes("dumanjug\t -> \tRoute 4.2.1","Moalboal\t -> \tRoute 4.2.2\n",18.9);
 
-               Routes route3_42 = new Routes("CarCar","Sibunga",10.2);
-               Routes route42_5 = new Routes("Sibunga","Argao",22.3);
-               Routes route5_51 = new Routes("Argao","Ronda",21.4);
-               Routes route51_52 = new Routes("Ronda","Moalboal",10.4);
+               Routes route3_42 = new Routes("CarCar\t\t -> \tRoute 3","Sibunga",10.2);
+               Routes route42_5 = new Routes("Sibunga\t\t -> \tRoute 4.2","Argao",22.3);
+               Routes route5_51 = new Routes("Argao\t\t -> \tRoute 5","Ronda",21.4);
+               Routes route51_52 = new Routes("Ronda\t\t -> \tRoute 5.1","Moalboalt\t -> \tRoute 5.2\n",10.4);
+
+         
             if(!(obstructed.contains("Barili"))){
                  routes.add(route1_2_3);
                  routes.add(route3_41);
                  routes.add(route41_411);
-                 routes.add(route411_412);
-               
+                 routes.add(route411_412);  
+
              }else if( !(obstructed.contains("Dumanjug"))){               
                  routes.add(route1_2_3);
                  routes.add(route3_42);
@@ -66,16 +74,17 @@ class RoutesDistance{
                 routes.add(route3_42);
                 routes.add(route42_5);
                 routes.add(route5_51);
-                routes.add(route51_52);
-              
+                routes.add(route51_52);            
              }
-               TOA =  computeTotalDistance(speedy,getDisTance());
+
+             displayRoutes();          
+             double TOA =  computeTotalDistance(speedy,getDisTance());
              System.out.println("Speed: " + speedy);
              double dd = getDisTance();
              System.out.println("Total Distance: "+dd + "km");
              int hour = (int)TOA/60;
              int minutes = (int)TOA%60;
-             System.out.println("Time of arrival: " + hour  +"hr/s,"+minutes + " minutes.");
+             System.out.println("Time of arrival: " + hour  +"hr/s,"+minutes + " minutes.\n");
     }
 }
 
@@ -95,11 +104,6 @@ public class RoutesToMoalboal{
   public static void main (String[]args){
  
           RoutesDistance routeDistance = new RoutesDistance();
-        
-          System.out.println("Cebu City (South Bus) ");
-          System.out.println("Route 1 Mingla");
-          System.out.println("");
-          
-          routeDistance.printRoutes();
+          routeDistance.bestRoutes();
   }
 }
