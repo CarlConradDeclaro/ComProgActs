9e7b4427b32353a1145fd85acdfc5b838a621e32
Carl Conrad C. Declaro <110441309+CarlConradDeclaro@users.noreply.github.com>
2023-10-11 20:24:36 +0800

    Update RoutesToMoalboal.java

--- 2nd Year Coding Journey/FastestRoutesToMoaboal/RoutesToMoalboal.java
+++ 2nd Year Coding Journey/FastestRoutesToMoaboal/RoutesToMoalboal.java
@@ -1,4 +1,75 @@
-   void getObstrutedRoutes(){                 
+ import java.util.*;
+
+class FindShorstestRoutes{
+        Scanner sc = new Scanner(System.in);
+        List<Routes> routes = new ArrayList<>();    
+        List<String> obstructed = new ArrayList<>();
+        private double speedy =0;
+
+     void findBestRoutes(){   
+
+               getObstrutedRoutes();
+               askSpeed();
+
+               Routes route1 = new Routes("SouthBus", "Mingla",  "Start",14.2);
+               Routes route2 = new Routes("Minglanilia", "San Fernando","Route 1",13.3);
+               Routes route3 = new Routes("San Fernando", "Carcar", "Route 2",10.8);
+               routes.add(route1);
+               routes.add(route2);
+               routes.add(route3);
+              
+               Routes route3_41 = new Routes("CarCar","barili","Route 3",17.8);
+               Routes route41_411 = new Routes("barili","dumanjug","Route 4.1",16.9);
+               Routes route411_412 = new Routes("Dumanjug","Moalboal\t-> \tRoute 4.1.2","Route 4.1.1",13.3);                                                                                                                                                                                                                                                                                                                                                            Routes route42_421 = new Routes("sibonga","dumanjug","Route 4.2",28.5);
+               Routes route421_422 = new Routes("dumanjug","Moalboal\t-> \tRoute 4.2.2","Route 4.2.1",18.9);
+
+               Routes route3_42 = new Routes("CarCar","Sibunga","Route 3",10.2);
+               Routes route42_5 = new Routes("Sibunga","Argao","Route 4.2",22.3);
+               Routes route5_51 = new Routes("Argao","Ronda","Route 5",21.4);
+               Routes route51_52 = new Routes("Ronda","Moalboal\t-> \tRoute 5.2","Route 5.1",10.4);
+        
+            if(!(obstructed.contains("Barili"))){   
+                 routes.add(route3_41);
+                 routes.add(route41_411);
+                 routes.add(route411_412);  
+
+             }else if( !(obstructed.contains("Dumanjug"))){                              
+                 routes.add(route3_42);
+                 routes.add(route42_421);
+                 routes.add(route421_422);              
+             }else {               
+                routes.add(route3_42);
+                routes.add(route42_5);
+                routes.add(route5_51);
+                routes.add(route51_52);            
+             }         
+    }  
+    
+
+    double computeTotalDistance(double speed, double dis){
+        return (dis/ speed  ) * 60;
+    }    
+
+    double getSpeedy(){
+        return speedy;
+    }  
+
+    int getHours(){
+        return (int)(computeTotalDistance(getSpeedy(),getDisTance()))/60; 
+    }  
+
+    int getMinutes(){
+        return (int)(computeTotalDistance(getSpeedy(),getDisTance()))%60;
+    }
+
+    double getDisTance(){
+        double dis=0;
+        for(Routes d : routes)
+             dis+=d.distance;
+        return dis;
+    }
+
+    void getObstrutedRoutes(){                 
              System.out.println("Is Barili obstructed? (y/n) ");
              String obs = sc.nextLine().toLowerCase();             
                 if(obs.equals("y")){
@@ -25,3 +96,29 @@
                System.out.println(r.distination+"\t"+r.distance+"km");   
         }       
     }
+}
+
+class Routes{
+        String startingPoint;
+        String distination;
+        String route;
+         double distance;
+         Routes(){}
+         Routes(String startingPoint,String distination,String route,double distance){
+             this.startingPoint = startingPoint ;
+             this.distination = distination;
+             this.route =route;
+             this.distance =distance;
+            }    
+}
+
+public class RoutesToMoalboal{
+  public static void main (String[]args){
+          FindShorstestRoutes fRoutes= new FindShorstestRoutes();
+          fRoutes.findBestRoutes();
+          fRoutes.displayRoutes();       
+          System.out.println("\nSpeed: " +fRoutes.getSpeedy());
+          System.out.println("Total Distance: "+fRoutes.getDisTance() + "km");
+          System.out.println("Time of arrival: " + fRoutes.getHours()  +"hr/s,"+fRoutes.getMinutes() + " minutes.\n");
+  }
+}
