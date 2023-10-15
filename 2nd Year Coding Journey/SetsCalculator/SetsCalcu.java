ae32a6505962b48130d6ba916fddc2521b6b4b41
carl conrad <carlconrad98071234@gmail.com>
2023-10-15 12:42:58 +0800

    sets calculator

--- 2nd Year Coding Journey/SetsCalculator/SetsCalcu.java
+++ 2nd Year Coding Journey/SetsCalculator/SetsCalcu.java
@@ -1,7 +1,9 @@
 package SetsCalculator;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 class Sets{  
@@ -20,22 +22,57 @@ class SetsElements{
     HashMap<String,List<String>> sets = new HashMap<>();
     Scanner sc = new Scanner(System.in); 
 
-    public void setSets(int s,int e){
-        for (int i = 0; i < s; i++) {
-            for (int j = 0; j < e; j++) {
-                
-            }
-        }
+    public List<String> setSets(){
+        List<String> elements = new ArrayList<>();
+
+        boolean run = true;
+         System.out.println("Enter -1 to stop");
+         System.out.println("Enter element: ");
+          
+        do{
+            String ans = sc.nextLine();
+           
+            if(!ans.equals("-1")){
+               elements.add(ans);
+            }else 
+               run = false;
+
+        }while(run);
+
+        return elements;
     }
     
     public void createSets(){
-        System.out.print("How many Sets do you want to make? : ");
-        int numOfSets = sc.nextInt();
-        
-        System.out.print("How many Element(s)? : ");
-        int numOfElements = sc.nextInt();
+       
+       boolean run = true; 
+      do{
+        System.out.print("Enter name of the set: "); 
+        String name = sc.nextLine();
+        sets.put(name,setSets());
 
-          
+        System.out.print("Do you want to Create another Set? (y/n) ");
+        String ans = sc.nextLine();
+        if(ans.equals("n"))
+          run = false;
+
+      }while(run);
+        
+         
+
+    }
+
+    public void displaySets(){
+
+      for(Map.Entry<String,List<String>> hashmap : sets.entrySet()){
+          String key = hashmap.getKey();
+          List<String> values = hashmap.getValue();
+          System.out.print(key + " = ");
+          System.out.print("{ ");
+            for (String value : values) {
+                System.out.print(value + " ");
+            }
+          System.out.println(" }");
+      }
 
     }
 
@@ -43,11 +80,10 @@ class SetsElements{
 }
 
 
-
-
-
 public class SetsCalcu {
     public static void main(String[] args) {
-        
+        SetsElements setElements = new SetsElements();
+        setElements.createSets();
+        setElements.displaySets();
     }
 }
