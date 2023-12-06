16b979465d02a0ad15385c149a2486092f00c3b5
Carl Conrad C. Declaro <110441309+CarlConradDeclaro@users.noreply.github.com>
2023-12-06 11:37:37 +0800

    Update postfixWithEvaluation.java

--- 2nd Year Coding Journey/Postfix with evaluation/postfixWithEvaluation.java
+++ 2nd Year Coding Journey/Postfix with evaluation/postfixWithEvaluation.java
@@ -1,32 +1,37 @@
 import java.util.Stack;
 import java.util.ArrayList;
 import java.util.Scanner;
-/*
-author: carlconradeclaro
-*/
 
-class PMDAS{
-      static Stack<Integer> values = new Stack<>();
-      static Stack<Character> operators = new Stack<>();
-      static StringBuilder posfix = new StringBuilder();
-      static ArrayList<String> evalutaion = new ArrayList<>();
+class PMDAS {
+    static Stack<Integer> values = new Stack<>();
+    static Stack<Character> operators = new Stack<>();
+    static StringBuilder posfix = new StringBuilder();
+    static ArrayList<String> evaluation = new ArrayList<>();
+
     public static int calculatePMDAS(String expression) {
-       
         for (int i = 0; i < expression.length(); i++) {
             char ch = expression.charAt(i);
 
             if (ch == ' ') {
                 continue; // Skip spaces
-            } else if (Character.isDigit(ch)) {
-                int num = ch - '0'; //  this ch will convert to int                   
-                while (i + 1 < expression.length() && Character.isDigit(expression.charAt(i + 1))) {                   
-                     num = num * 10 + (expression.charAt(i + 1) - '0');                    
+            } else if (Character.isDigit(ch) || (ch == '-' && i + 1 < expression.length() && Character.isDigit(expression.charAt(i + 1)))) {
+                int num;
+                if (ch == '-') {
+                    num = -1 * (expression.charAt(i + 1) - '0');
+                    i++;
+                } else {
+                    num = ch - '0';
+                }
+
+                while (i + 1 < expression.length() && (Character.isDigit(expression.charAt(i + 1)))) {
+                    num = num * 10 + (expression.charAt(i + 1) - '0');
                     i++;
                 }
+
                 values.push(num);
                 posfix.append(num);
             } else if (ch == '(') {
-                operators.push(ch);            
+                operators.push(ch);
             } else if (ch == ')') {
                 while (!operators.isEmpty() && operators.peek() != '(') {
                     posfix.append(operators.peek());
@@ -34,7 +39,7 @@ class PMDAS{
                     int operand2 = values.pop();
                     int operand1 = values.pop();
                     values.push(applyOperator(operator, operand1, operand2));
-                evalutaion.add(operand1 +" " + operator + " "+ operand2 + " = " +  applyOperator(operator, operand1, operand2));
+                    evaluation.add(operand1 + " " + operator + " " + operand2 + " = " + applyOperator(operator, operand1, operand2));
                 }
                 operators.pop(); // Pop the '('
             } else if (isOperator(ch)) {
@@ -42,12 +47,12 @@ class PMDAS{
                     posfix.append(operators.peek());
                     char operator = operators.pop();
                     int operand2 = values.pop();
-                    int operand1 = values.pop();              
+                    int operand1 = values.pop();
                     values.push(applyOperator(operator, operand1, operand2));
-                  evalutaion.add(operand1 +" " + operator + " "+ operand2 + " = " +  applyOperator(operator, operand1, operand2));
+                    evaluation.add(operand1 + " " + operator + " " + operand2 + " = " + applyOperator(operator, operand1, operand2));
                 }
                 operators.push(ch);
-            }  
+            }
         }
 
         while (!operators.isEmpty()) {
@@ -56,19 +61,18 @@ class PMDAS{
             int operand2 = values.pop();
             int operand1 = values.pop();
             values.push(applyOperator(operator, operand1, operand2));
-          
-          evalutaion.add(operand1 +" " + operator + " "+ operand2 + " = " +  applyOperator(operator, operand1, operand2));
-        }     
+            evaluation.add(operand1 + " " + operator + " " + operand2 + " = " + applyOperator(operator, operand1, operand2));
+        }
         return values.pop();
     }
 
-    String getPostfix(){
+    String getPostfix() {
         return posfix.toString();
     }
 
-    void displauEvaluation(){
-        for(String x : evalutaion)
-         System.out.println(x);
+    void displayEvaluation() {
+        for (String x : evaluation)
+            System.out.println(x);
     }
 
     private static boolean isOperator(char ch) {
@@ -87,9 +91,10 @@ class PMDAS{
                 return 0;
         }
     }
+
     private static int applyOperator(char operator, int operand1, int operand2) {
-         int val =0;
-          switch (operator) {
+        int val = 0;
+        switch (operator) {
             case '+':
                 val = operand1 + operand2;
                 break;
@@ -101,16 +106,16 @@ class PMDAS{
                 break;
             case '/':
                 if (operand2 != 0) {
-                    val =  operand1 / operand2;
+                    val = operand1 / operand2;
                 }
                 break;
         }
-           return val;
+        return val;
     }
 }
 
 class pmdasStack {
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         PMDAS pmdas = new PMDAS();
@@ -118,8 +123,8 @@ class pmdasStack {
         String expression = sc.nextLine();
         int result = pmdas.calculatePMDAS(expression);
         System.out.println("\nPostfix: " + pmdas.getPostfix());
-        System.out.println("test: ");
-        pmdas.displauEvaluation();
-        System.out.println("\nRESULT: " + result +"\n");
+        System.out.println("Evaluation: ");
+        pmdas.displayEvaluation();
+        System.out.println("\nRESULT: " + result + "\n");
     }
 }
